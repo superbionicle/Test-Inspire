74ae26cccd1474a683bc09260b42bf55a27b6d80
Arthur <arthurlecocq@gmail.com>
2022-05-04 21:34:04 +0200

    Ecriture des attentes du client

--- Inspire/Inspire/main.cpp
+++ Inspire/Inspire/main.cpp
@@ -7,6 +7,32 @@
 
 #include <iostream>
 
+/*
+ 
+Attentes du client :
+ Faire un jeu quizz avec des cartes
+ Chaque carte aura :
+    - un thème
+    - une question
+    - 1 réponse correcte et entre 1 et 3 réponses fausses
+ Ce qu'il faut faire :
+    - créer un programme controlable via le terminal
+        V1 : controlable avec l'écriture
+        V2 : controlable avec le clavier (flèches directionnelles et echap en plus pour quitter)
+    - permettre de créer un thème pouvant regrouper plusieurs cartes
+    - faire une session de révision par thème (sans score en jeu)
+    - faire une session de jeu avec tous les thèmes et avec score en jeu
+    - permettre de rajouter des cartes d'apprentissage dans un fichier
+        écriture et lecture d'un fichier
+        format pour le faire ?
+    - permettre de compter les points pour chaque manche gagner, les afficher au fur et à mesure de la partie
+    - faire un menu des high scores
+        écriture et lecture d'un fichier des scores
+        format : txt ? (nom ...... score)
+ 
+ 
+*/
+
 int main(){
     return 0;
 }
