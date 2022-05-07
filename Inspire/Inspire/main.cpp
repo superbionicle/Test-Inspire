fed518319474a52add915497888ce1538867d723
Arthur <arthurlecocq@gmail.com>
2022-05-07 16:49:41 +0200

    Restructuration

--- Inspire/Inspire/main.cpp
+++ Inspire/Inspire/main.cpp
@@ -5,71 +5,6 @@
 //  Created by Arthur on 04/05/2022.
 //
 
-
-/*
- 
- Attentes du client :
- Faire un jeu quizz avec des cartes
- Chaque carte aura :
-    - un thème
-    - une question
-    - 1 réponse correcte et entre 1 et 3 réponses fausses
- Ce qu'il faut faire :
-    - créer un programme controlable via le terminal
-        V1 : controlable avec l'écriture
-        V2 : controlable avec le clavier (flèches directionnelles et echap en plus pour quitter)
-    - permettre de créer un thème pouvant regrouper plusieurs cartes
-    - faire une session de révision par thème (sans score en jeu)
-    - faire une session de jeu avec tous les thèmes et avec score en jeu
-    - permettre de rajouter des cartes d'apprentissage dans un fichier
-        écriture et lecture d'un fichier
-        format pour le faire ?
-    - permettre de compter les points pour chaque manche gagner, les afficher au fur et à mesure de la partie
-    - faire un menu des high scores
-        écriture et lecture d'un fichier des scores
-        format : txt ? (classement ... nom ...... score)
- 
- Remarque : trouver le moyen de clear la console terminal pour avoir un affichage propre à chaque fois qu'on se balade dans les menus
- Bonus : essayer de mettre en place un chrono en arrière plan et donner le temps à la fin d'une session de jeu
- 
- Conception de la structure :
- Menu :
-    1. Jouer une partie (tout thème confondu)
-    2. Lancer une partie de révision
-        2.1. Choisir un thème
-        2.2 Retour au menu précédent
-    3. Ajouter, modifier ou retirer une carte
-        3.1. Ajouter une carte au deck
-            Choisir le thèe parmis ceux existant
-            Entrer la question
-            Est-ce une question vrai/faux ?
-            Si non, alors :
-                Entrer la bonne réponse
-                Choisir le nombre de mauvaises réponses
-                Entrer les mauvaises réponses
-            Ajouter une nouvelle carte ? oui/non
-            Si oui
-                On boucle
-            Si non
-                Retour au menu précédent
-            Bouton "Annuler" toujours précédent pendant l'ajout
-        3.2. Modifier une carte existante
-        3.3. Supprimer une carte existante
-        3.4. Retour au menu précédent
-    4. Voir les high scores
-        4.1. (Affichage des 10 meilleurs)
-        4.2. Retour au menu précédent
-    5. Créer un nouveau thème pour le deck
-    0. Quitter (Echap pour quitter)
- 
- Etapes :
-    1. Faire le menu
-    2. Créer les classes "Carte" et "Thème"
-    3. Faire les options du menu pour ajouter cartes et thèmes
-    4. Trouver la manière de faire de l'écriture et de la lecture de fichier pour enregistrer des données
- 
-*/
-
 #include <iostream>
 #include <vector>
 #include "carte.hpp"
@@ -80,6 +15,7 @@ using namespace std;
 
 
 int main(){
+    
     vector<carte> liste_cartes;
     vector<theme> liste_theme;
     int choix_menu=0;
