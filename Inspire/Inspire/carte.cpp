126dad4c3761477772422e4d3ab02fbcd4ff9605
Arthur <arthurlecocq@gmail.com>
2022-05-08 11:36:21 +0200

    Fin de l'importation manuelle des modifs

--- Inspire/Inspire/carte.cpp
+++ Inspire/Inspire/carte.cpp
@@ -10,13 +10,6 @@
 #include <string.h>
 using namespace std;
 
-carte::carte(){
-    
-}
-carte::~carte(){
-    
-}
-
 void carte::set_question(){
     string question_temp;
     cout<<"Quel est votre questio ?"<<endl;
@@ -26,38 +19,50 @@ void carte::set_question(){
 
 void carte::set_reponses(){
     cout<<"Rappel de la question : "<<question<<endl;
-    string reponse_bool;
-    do{ // On cherche à savoir s'il s'agit d'un vrai/faux
-        cout<<"Est ce une question vrai/faux ? "<<endl;
-        cout<<"Si oui, tapez \"oui\""<<endl;
-        cout<<"Si non, tapez \"non\""<<endl;
-        cin>>reponse_bool;
-    }while(reponse_bool!="oui" && reponse_bool!="non"); // On boucle tant qu'on a pas la bonne réponse
-    if(reponse_bool=="oui"){ // S'il s'agit d'un vrai/faux, on met directement les réponses
-        reponses.push_back("Vrai");
-        reponses.push_back("Faux");
+    int nb_erreurs=1;
+    string reponse_correcte;
+    cout<<"Saisissez la bonne réponse à votre question :"<<endl;
+    cin>>reponse_correcte; // L'user entre d'abord la bonne réponse
+    reponses.push_back(reponse_correcte); // On ajoute la bonne réponse à la liste des réponses possibles
+    do{
+        cout<<"Combien de mauvaises réponses voulez vous (entre 1 et 3) : ";
+        cin>>nb_erreurs; // On demande combien de mauvaises réponses l'user souhaite-il (entre 1 et 3)
+    }while(nb_erreurs<1 || nb_erreurs>3); // On boucle tant qu'on a pas la bonne réponse
+    for(int i=0;i<nb_erreurs;i++){ // Pour le nombre souhaité, on lui fait rentrer les mauvaises réponses
+        string erreur;
+        cout<<"Saisissez une mauvaise réponse à votre question : "<<endl;
+        cin>>erreur; // L'user entre une mauvaise réponse
+        reponses.push_back(erreur); // On ajoute la mauvaise réponse à la liste des réponses possibles
     }
-    else{// Sinon, on demande à l'utilisateur d'entrer les réponses lui même
-        int nb_erreurs=1;
-        string reponse_correcte;
-        cout<<"Saisissez la bonne réponse à votre question (entre 1 et 3) :"<<endl;
-        cin>>reponse_correcte; // L'user entre d'abord la bonne réponse
-        reponses.push_back(reponse_correcte); // On ajoute la bonne réponse à la liste des réponses possibles
-        do{
-            cout<<"Combien de mauvaises réponses voulez vous : ";
-            cin>>nb_erreurs; // On demande combien de mauvaises réponses l'user souhaite-il (entre 1 et 3)
-        }while(nb_erreurs<0 && nb_erreurs>3); // On boucle tant qu'on a pas la bonne réponse
-        for(int i=0;i<nb_erreurs;i++){ // Pour le nombre souhaité, on lui fait rentrer les mauvaises réponses
-            string erreur;
-            cout<<"Saisissez une mauvaise réponse à votre question : "<<endl;
-            cin>>erreur; // L'user entre une mauvaise réponse
-            reponses.push_back(erreur); // On ajoute la mauvaise réponse à la liste des réponses possibles
-        }
-        cout<<"Fin de l'édition de la question et de ses réponses"<<endl;
-    }
+    cout<<"Fin de l'édition de la question et de ses réponses"<<endl;
     
 }
-bool carte::lancer_question(){
-    bool reussite=false;
-    return(reussite);
+
+vector<string> carte::get_reponses(){
+    vector<string> reponses_question;
+    for(int i=0;i<reponses.size();i++){
+        reponses_question.push_back(reponses[i]);
+    }
+    return(reponses_question);
+}
+
+void carte::set_reponses(int nb,vector<string> rep){
+    for(int i=0;i<nb;i++){
+        reponses.push_back(rep[i]);
+    }
+}
+
+void carte::display(){
+    cout<<"Thème : "<<theme_carte<<endl;
+    cout<<"Question : "<<question<<endl;
+    for(int i=0;i<reponses.size();i++){
+        cout<<"Reponse "<<i+1<<" : "<<reponses[i]<<endl;
+    }
+    cout<<endl;
+}
+
+void carte::display_rep(){
+    for(int i=0;i<reponses.size();i++){
+        cout<<"Reponse "<<i+1<<" : "<<reponses[i]<<endl;
+    }
 }
--- Inspire/Inspire/carte.hpp
+++ Inspire/Inspire/carte.hpp
@@ -15,18 +15,30 @@ using namespace std;
 
 
 class carte{
-    string theme_carte;
-    string question;
-    bool vrai_faux=true;
-    vector<string> reponses={"Vrai","Faux"};
+    string theme_carte="";
+    string question="";
+    vector<string> reponses;
 public:
-    carte();
-    ~carte();
+    // Constructeur et destructeur
+    //carte();
+    ~carte(){reponses.clear();};
+    
+    // Méthodes pour définir les attributs
     void set_theme(string nom_theme){theme_carte=nom_theme;};
     void set_question(string question_a_donner){question=question_a_donner;};
     void set_question();
     void set_reponses();
-    bool lancer_question();
+    void set_reponses(int nb,vector<string> rep);
+    
+    // Méthodes pour récupérer les attributs
+    int get_nb(){return(reponses.size());}
+    string get_theme(){return(theme_carte);};
+    string get_question(){return(question);};
+    vector<string> get_reponses();
+    
+    // Méthode d'affichage
+    void display();
+    void display_rep();
 };
 
 #endif /* carte_hpp */
